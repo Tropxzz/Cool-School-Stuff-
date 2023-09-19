6f48f7e2019b4f322c16745dbdd8fcacb6ccafad
Tropxzz <130433432+Tropxzz@users.noreply.github.com>
2023-09-19 16:57:54 +1000

    Update SetCrypto.js

--- Blooket/CyptoHack/SetCrypto.js
+++ Blooket/CyptoHack/SetCrypto.js
@@ -1,16 +1,1 @@
-var wpm = parseInt(prompt("How much Crypto do u want??"));
-
-if (isNaN(wpm)) {
-  alert("Please enter a valid number. Try again, please.");
-} else {
-  stateNode.setState({ crypto: wpm, crypto2: wpm });
-
-  stateNode.props.liveGameController.setVal({
-    path: "c/" + stateNode.props.client.name,
-    val: {
-      b: stateNode.props.client.blook,
-      p: stateNode.state.password,
-      cr: wpm,
-    },
-  });
-}
+// not done 
