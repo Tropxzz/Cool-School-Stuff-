038ac14eefed4b49844a7902918bd82fa522d997
Tropxzz <130433432+Tropxzz@users.noreply.github.com>
2023-09-20 16:26:28 +1000

    Update SetCrypto.js

--- Blooket/CyptoHack/SetCrypto.js
+++ Blooket/CyptoHack/SetCrypto.js
@@ -1,1 +1,18 @@
-// not done 
+amount = 100000000 // amount here
+
+                        stateNode.setState({ crypto: amount, crypto2: amount });
+                        stateNode.props.liveGameController.setVal({
+                            path: "c/".concat(stateNode.props.client.name),
+                            val: {
+                                b: stateNode.props.client.blook,
+                                cr: amount
+                            }
+                        });                        stateNode.setState({ crypto: amount, crypto2: amount });
+                        stateNode.props.liveGameController.setVal({
+                            path: "c/".concat(stateNode.props.client.name),
+                            val: {
+                                b: stateNode.props.client.blook,
+                                p: stateNode.state.password,
+                                cr: amount
+                            }
+                        });
