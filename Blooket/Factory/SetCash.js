e00625b5359dde18aa3bd0e5f41da1929c0a7ab8
Tropxzz <130433432+Tropxzz@users.noreply.github.com>
2023-09-20 16:27:17 +1000

    Create SetCash.js

---
+++ Blooket/Factory/SetCash.js
@@ -0,0 +1,2 @@
+cash = 10000000000000000000000000000 // amount
+Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner.stateNode.setState({ cash });
